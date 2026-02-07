a1ea891980e2a4fe9a8db286ad8dbb216ec764a9
Kurt Duncan <kurtaduncan@gmail.com>
2026-02-07 16:04:08 -0700

    Add JSON serialization/deserialization to SocketTrace object

--- netlib/src/main/com/bearsnake/komodo/netlib/SocketTrace.java
+++ netlib/src/main/com/bearsnake/komodo/netlib/SocketTrace.java
@@ -4,10 +4,46 @@
 
 package com.bearsnake.komodo.netlib;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.LinkedList;
 
 public class SocketTrace {
+
+    public static class HexSerializer extends JsonSerializer<byte[]> {
+        @Override
+        public void serialize(byte[] value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+            StringBuilder sb = new StringBuilder();
+            for (byte b : value) {
+                sb.append(String.format("%02X", b));
+            }
+            gen.writeString(sb.toString());
+        }
+    }
+
+    public static class HexDeserializer extends JsonDeserializer<byte[]> {
+        @Override
+        public byte[] deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+            String s = p.getValueAsString();
+            if (s == null || s.isEmpty()) {
+                return new byte[0];
+            }
+            byte[] bytes = new byte[s.length() / 2];
+            for (int i = 0; i < bytes.length; i++) {
+                bytes[i] = (byte) Integer.parseInt(s.substring(i * 2, i * 2 + 2), 16);
+            }
+            return bytes;
+        }
+    }
 
     public enum Source {
         LOCAL,
@@ -20,23 +56,60 @@ public class SocketTrace {
         private final Source _source;
         private final byte[] _data;
 
+        @JsonCreator
+        public Entry(@JsonProperty("source") final Source source,
+                     @JsonProperty("data") @JsonDeserialize(using = HexDeserializer.class) final byte[] data,
+                     @JsonProperty("timestamp") final long timestamp) {
+            _source = source;
+            _data = data;
+            _timestamp = timestamp;
+        }
+
         private Entry(final Source source, final byte[] data) {
             _timestamp = System.currentTimeMillis();
             _source = source;
             _data = data;
         }
 
+        @JsonProperty("timestamp")
         public long getTimestamp() { return _timestamp; }
+
+        @JsonProperty("source")
         public Source getSource() { return _source; }
+
+        @JsonProperty("data")
+        @JsonSerialize(using = HexSerializer.class)
         public byte[] getData() { return _data; }
+    }
+
+    private final LinkedList<Entry> _entries = new LinkedList<>();
+    private int _maxEntries = DEFAULT_MAX_ENTRIES;
+
+    @JsonCreator
+    public SocketTrace(@JsonProperty("maxEntries") int maxEntries,
+                       @JsonProperty("entries") LinkedList<Entry> entries) {
+        _maxEntries = maxEntries;
+        if (entries != null) {
+            _entries.addAll(entries);
+        }
+    }
+
+    public SocketTrace() {
+    }
+
+    @JsonProperty("maxEntries")
+    public int getMaxEntries() {
+        return _maxEntries;
+    }
+
+    @JsonProperty("entries")
+    public LinkedList<Entry> getEntriesForJson() {
+        return getEntries();
     }
 
     public static final int DEFAULT_MAX_ENTRIES = 1000;
     public static final int MIN_MAX_ENTRIES = 100;
     public static final int MAX_MAX_ENTRIES = 10000;
-
-    private final LinkedList<Entry> _entries = new LinkedList<>();
-    private int _maxEntries = DEFAULT_MAX_ENTRIES;
 
     public void addEntry(final Source source,
                          final byte[] data) {
@@ -79,5 +152,16 @@ public class SocketTrace {
         synchronized (_entries) {
             return new LinkedList<>(_entries);
         }
+    }
+
+    public static SocketTrace loadFromFile(File file) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(file, SocketTrace.class);
+    }
+
+    public void saveToFile(File file) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        mapper.writeValue(file, this);
     }
 }
